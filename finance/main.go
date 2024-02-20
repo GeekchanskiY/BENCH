9a57a6e9c398a976673846a6a3b2434189265fc2
GeekchanskiY <astrovskiy.04@gmail.com>
2024-02-20 16:42:40 +0400

    sample playground

--- finance/main.go
+++ finance/main.go
@@ -1,13 +1,38 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func LoggerMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+		c.Next()
+		duration := time.Since(start)
+		log.Printf("Request - Method: %s | Status: %d | Duration: %v", c.Request.Method, c.Writer.Status(), duration)
+	}
+}
 
 func main() {
 	r := gin.Default()
+	r.Use(LoggerMiddleware())
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	v1 := r.Group("/v1")
+	v2 := v1.Group("/v12")
+
+	{
+		v2.GET("/ping", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "pong",
+			})
+		})
+	}
+	r.Run(":8085")
 }
