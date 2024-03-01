a234f341744ad749fc50dc2f7276f526d7980e9c
GeekchanskiY <astrovskiy.04@gmail.com>
2024-03-01 12:10:52 +0400

    upd

--- coordinator/src/alembic/env.py
+++ coordinator/src/alembic/env.py
@@ -2,7 +2,7 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-from repos.db import Base
+from models.db import Base
 from alembic import context
 
 # this is the Alembic Config object, which provides
