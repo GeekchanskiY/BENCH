7dd9ee88d4dc4db4a3e1a5a281e25f765ef29600
GeekchanskiY <astrovskiy.04@gmail.com>
2024-03-06 10:35:58 +0400

    tmp upd

--- frontend/app/src/features/jwt/Jwt.jsx
+++ frontend/app/src/features/jwt/Jwt.jsx
@@ -7,6 +7,24 @@ import { login, logout } from "./jwtSlice";
 export default function Jwt(){
     const jwt = useSelector((state) => state.jwt.token)
 
+    function login(){
+        fetch(
+            'http://0.0.0.0:80/healthcheck',
+            {
+                method: 'POST',
+                body: JSON.stringify({
+                    'email': 'SampleUserEmail',
+                    'passoword': 'SampleStrongPassword'
+                })
+                
+            }
+        )
+        .then(res => res.json())
+        .then(data => {
+            
+        })
+    }
+
     return <div>
         {jwt == null ? "null" : jwt}
     </div>
