f65eae41b14bd30947af732c171d893a9c9c2c5c
GeekchanskiY <astrovskiy.04@gmail.com>
2024-03-08 13:28:50 +0400

    delete service ready, privileges check added to create service

--- coordinator/src/routers/services.py
+++ coordinator/src/routers/services.py
@@ -3,15 +3,37 @@ from fastapi import APIRouter, Depends, HTTPException, Request
 from .depends import get_service_service
 from services.serviceService import ServiceService
 from schemas.serviceSchema import ServiceSchema, ServiceLogSchema, ServiceWithLogsSchema
+from schemas.statusSchemas import MessageResponseSchema
+
+from .depends import JWTBearer, get_jwt_bearer, JWTCredentials
+
 
 router: APIRouter = APIRouter()
 
 @router.get("/", tags=["services"], response_model=list[ServiceSchema])
-async def read_services(service: ServiceService = Depends(get_service_service)):
+async def read_services(
+    service: ServiceService = Depends(get_service_service),
+    ):
     services = await service.get_services()
     return services
 
 @router.post('/create', tags=['services'], response_model=ServiceSchema)
-async def create_service(service_data: ServiceSchema, service: ServiceService = Depends(get_service_service)):
-    created_service = await service.create_service(service_data)
+async def create_service(
+    service_data: ServiceSchema,
+    service: ServiceService = Depends(get_service_service),
+    credentials: JWTCredentials = Depends(JWTBearer())):
+
+    created_service = await service.create_service(credentials.username, service_data)
     return created_service
+
+@router.delete('/service_id/{service_id}', tags=['services'], response_model=MessageResponseSchema)
+async def delete_service(
+    service_id: int,
+    service: ServiceService = Depends(get_service_service),
+    credentials: JWTCredentials = Depends(JWTBearer())):
+    
+    try:
+        return await service.delete_service(service_id, credentials.username)
+    except Exception as e:
+        raise HTTPException(404, str(e))
+    
--- coordinator/src/services/serviceService.py
+++ coordinator/src/services/serviceService.py
@@ -1,16 +1,38 @@
 from repositories.serviceRepository import ServiceRepository
 from schemas.serviceSchema import ServiceLogSchema, ServiceSchema, ServiceWithLogsSchema
+from schemas.userSchema import UserSchema
+from schemas.statusSchemas import MessageResponseSchema
+from repositories.userRepository import UserRepository
 
 class ServiceService:
     service_repository: ServiceRepository
+    user_repository: UserRepository
 
     def __init__(self) -> None:
         self.service_repository = ServiceRepository()
+        self.user_repository = UserRepository()
     
-    async def create_service(self, servicedata: ServiceSchema) -> ServiceSchema:
+    async def create_service(self, username: str, servicedata: ServiceSchema) -> ServiceSchema:
+        user: UserSchema = self.user_repository.get_user_by_name(username)
+        
+        if not user.is_staff:
+            raise Exception('Not enough privileges!')
+        
         service = self.service_repository.create_service(servicedata)
         return service
     
     async def get_services(self) -> list[ServiceSchema]:
         services: list[ServiceSchema] = self.service_repository.get_services()
         return services
+    
+    async def delete_service(self, service_id: int, username: str) -> MessageResponseSchema:
+        user: UserSchema = self.user_repository.get_user_by_name(username)
+        
+        if not user.is_staff:
+            raise Exception('Not enough privileges!')
+        
+        deleted = self.service_repository.delete_service(service_id)
+        if deleted:
+            return MessageResponseSchema(msg='Deleted')
+        else:
+            return MessageResponseSchema(msg='Failed to delete')
