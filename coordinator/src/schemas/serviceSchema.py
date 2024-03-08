5e0c6e90204505389420bc5bac837879cad34cfd
GeekchanskiY <astrovskiy.04@gmail.com>
2024-03-08 12:47:06 +0400

    service model update

--- coordinator/src/schemas/serviceSchema.py
+++ coordinator/src/schemas/serviceSchema.py
@@ -3,12 +3,14 @@ from pydantic import BaseModel, ConfigDict, validator
 class ServiceSchema(BaseModel):
     model_config: ConfigDict = ConfigDict(from_attributes=True)
 
+    id: int | None
+
     name: str
     description: str
     is_active: bool
 
-    url: str
-    ping_url: str
+    url: str | None
+    ping_url: str | None
 
 
 class ServiceLogSchema(BaseModel):
