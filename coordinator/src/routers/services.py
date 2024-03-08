1ad95987d732a2b2754fc756c51a7cd97be86732
GeekchanskiY <astrovskiy.04@gmail.com>
2024-03-08 14:05:57 +0400

    updated

--- coordinator/src/routers/services.py
+++ coordinator/src/routers/services.py
@@ -5,7 +5,7 @@ from services.serviceService import ServiceService
 from schemas.serviceSchema import ServiceSchema, ServiceLogSchema, ServiceWithLogsSchema
 from schemas.statusSchemas import MessageResponseSchema
 
-from .depends import JWTBearer, get_jwt_bearer, JWTCredentials
+from .depends import get_jwt_bearer, JWTCredentials
 
 
 router: APIRouter = APIRouter()
@@ -21,16 +21,16 @@ async def read_services(
 async def create_service(
     service_data: ServiceSchema,
     service: ServiceService = Depends(get_service_service),
-    credentials: JWTCredentials = Depends(JWTBearer())):
+    credentials: JWTCredentials = Depends(get_jwt_bearer())):
 
     created_service = await service.create_service(credentials.username, service_data)
     return created_service
 
-@router.delete('/service_id/{service_id}', tags=['services'], response_model=MessageResponseSchema)
+@router.delete('/service/{service_id}', tags=['services'], response_model=MessageResponseSchema)
 async def delete_service(
     service_id: int,
     service: ServiceService = Depends(get_service_service),
-    credentials: JWTCredentials = Depends(JWTBearer())):
+    credentials: JWTCredentials = Depends(get_jwt_bearer())):
     
     try:
         return await service.delete_service(service_id, credentials.username)
