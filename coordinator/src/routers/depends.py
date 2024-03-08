02d331e6f92cb375683b460d06bade2d12d96816
GeekchanskiY <astrovskiy.04@gmail.com>
2024-03-08 13:28:18 +0400

    moved JWT DI class to dependencies, middleware removed

--- coordinator/src/routers/depends.py
+++ coordinator/src/routers/depends.py
@@ -1,5 +1,55 @@
 from services.userService import UserService
 from services.serviceService import ServiceService
+from fastapi import Request, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
+from utils.jwt_utils import decodeJWT
+from typing import Tuple
+
+
+class JWTCredentials(HTTPAuthorizationCredentials):
+    username: str
+
+
+class JWTBearer(HTTPBearer):
+    def __init__(self, auto_error: bool = True):
+        super(JWTBearer, self).__init__(auto_error=auto_error)
+
+    async def __call__(self, request: Request) -> JWTCredentials:
+        credentials: HTTPAuthorizationCredentials = await super(JWTBearer, self).__call__(request)
+        if credentials:
+            if not credentials.scheme == "Bearer":
+                raise HTTPException(status_code=403, detail="Invalid authentication scheme.")
+            
+            is_verified, username = self.verify_jwt(credentials.credentials)
+            if not is_verified:
+                raise HTTPException(status_code=403, detail="Invalid token or expired token.")
+            
+            
+            return JWTCredentials(
+                credentials=credentials.credentials,
+                scheme=credentials.scheme,
+                username=username
+            )
+        else:
+            raise HTTPException(status_code=403, detail="Invalid authorization code.")
+
+    def verify_jwt(self, jwtoken: str) -> Tuple[bool, str | None]:
+        
+        try:
+            payload = decodeJWT(jwtoken)
+        except:
+            payload = None
+        
+        if payload:
+            return True, payload.get('username')
+        else:
+            return False, None
+
+jwtBearer = JWTBearer()
+
+def get_jwt_bearer():
+    return jwtBearer()
 
 user_service = UserService()
 service_service = ServiceService()
