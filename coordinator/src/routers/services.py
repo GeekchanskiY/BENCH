5b62ed4a2532ab067abb92d787c71a35a4813b05
GeekchanskiY <astrovskiy.04@gmail.com>
2024-03-08 10:17:20 +0400

    create service added

--- coordinator/src/routers/services.py
+++ coordinator/src/routers/services.py
@@ -10,3 +10,8 @@ router: APIRouter = APIRouter()
 async def read_services(service: ServiceService = Depends(get_service_service)):
     services = await service.get_services()
     return services
+
+@router.post('/create', tags=['services'], response_model=ServiceSchema)
+async def create_service(service_data: ServiceSchema, service: ServiceService = Depends(get_service_service)):
+    created_service = await service.create_service(service_data)
+    return created_service
