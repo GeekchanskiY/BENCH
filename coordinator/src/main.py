19193cfb7c36500d080065ae08a24f1051329893
GeekchanskiY <astrovskiy.04@gmail.com>
2024-03-14 06:30:43 +0400

    updated cors

--- coordinator/src/main.py
+++ coordinator/src/main.py
@@ -18,7 +18,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*', 'http://0.0.0.0:3002'],
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allow_headers=['*']
