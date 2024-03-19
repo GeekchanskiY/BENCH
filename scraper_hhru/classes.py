d01d1b801cc07085913a418ff4895b742bb2527f
GeekchanskiY <astrovskiy.04@gmail.com>
2024-03-19 10:32:04 +0400

    Request config init

--- scraper_hhru/classes.py
+++ scraper_hhru/classes.py
@@ -1,6 +1,45 @@
 from dataclasses import dataclass
 
 class Query:
+
+    available_query_args: frozenset = frozenset((
+        'page', # int
+        'search_field', # values are in available_search_fields
+        'enable_snippets', # false by default
+        'part_time', # available_part_time
+        'only_with_salary', # bool
+        'salary', # ?int, 50K, 100K, 150K, 200K, 250K, custom
+        'area', # int 
+        'L_save_area', # bool
+        'experience',
+        'schedule', # str
+    ))
+
+    available_search_fields: frozenset = frozenset((
+        'name', 'company_name', 'description'
+    ))
+
+    available_part_time: frozenset = frozenset((
+        'employment_part', # Not full day
+        'start_after_sixteen', # Evening job
+        'only_saturday_and_sunday', # Weekend job
+        'employment_project', # Project job
+        'from_four_to_six_hours_in_a_day', # 4-6 h/day
+    ))
+
+    available_experience: frozenset = frozenset((
+        'between1And3',
+        'between3And6',
+        'moreThan6',
+        'noExperience',
+    ))
+
+    available_schedule: frozenset = frozenset((
+        'remote',
+        'fullDay',
+        'shift',
+    ))
+
     def __init__(self) -> None:
         pass
 
---
+++ scraper_hhru/config.py
@@ -0,0 +1,5 @@
+DEV: bool = True
+
+SEARCH_STRING: str = 'https://hh.ru/search/vacancy'
+ITEMS_PER_ITERATION: int = 5
+DELAY: int = 1
