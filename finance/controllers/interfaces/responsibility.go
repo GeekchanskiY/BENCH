276c9ee62af3b2e446540c6c834da290180bce58
GeekchanskiY <astrovskiy.04@gmail.com>
2024-04-02 23:31:21 +0400

    responsibility controller ready

---
+++ finance/controllers/interfaces/responsibility.go
@@ -0,0 +1,23 @@
+package interfaces
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type ResponsibilityController interface {
+	// Responsibility
+	FindAll(ctx *gin.Context)
+	FindByID(ctx *gin.Context)
+	Create(ctx *gin.Context)
+	Delete(ctx *gin.Context)
+
+	// Responsibility Synonims
+	CreateSynonim(ctx *gin.Context)
+	DeleteSynonim(ctx *gin.Context)
+	FindAllSynonims(ctx *gin.Context)
+
+	// Responsibility Conflict
+	CreateRespConflict(ctx *gin.Context)
+	DeleteRespConflict(ctx *gin.Context)
+	FindAllRespConflicts(ctx *gin.Context)
+}
---
+++ finance/controllers/responsibility.go
@@ -0,0 +1,170 @@
+package controllers
+
+import (
+	controller "Finance/controllers/interfaces"
+	interfaces "Finance/repositories/interfaces"
+	"Finance/schemas"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type respController struct {
+	respRepository interfaces.ResponsibilityRepository
+}
+
+func NewResponsibilityController(repo interfaces.ResponsibilityRepository) controller.ResponsibilityController {
+	return &respController{
+		respRepository: repo,
+	}
+}
+
+func (c *respController) FindAll(ctx *gin.Context) {
+	resps, err := c.respRepository.FindAll()
+	if err != nil {
+		ctx.JSON(400, ctx.Error(err))
+		return
+	}
+	ctx.JSON(200, resps)
+}
+
+func (c *respController) FindByID(ctx *gin.Context) {
+	var params_id string = ctx.Params.ByName("id")
+	i, err := strconv.ParseUint(params_id, 10, 32)
+	if err != nil {
+		ctx.JSON(400, ctx.Error(err))
+		return
+
+	}
+	var uid uint = uint(i)
+	resp, err := c.respRepository.FindByID(uid)
+	if err != nil {
+		ctx.JSON(400, ctx.Error(err))
+		return
+	}
+	ctx.JSON(200, resp)
+
+}
+
+func (c *respController) Create(ctx *gin.Context) {
+	var resp schemas.ResponsibilitySchema
+	err := ctx.BindJSON(&resp)
+	if err != nil {
+		ctx.JSON(400, ctx.Error(err))
+		return
+	}
+	resp, err = c.respRepository.Create(resp)
+	if err != nil {
+		ctx.JSON(400, ctx.Error(err))
+		return
+	}
+	ctx.JSON(200, resp)
+}
+
+func (c *respController) Delete(ctx *gin.Context) {
+	var params_id string = ctx.Params.ByName("id")
+	i, err := strconv.ParseUint(params_id, 10, 32)
+	if err != nil {
+		ctx.JSON(400, ctx.Error(err))
+		return
+	}
+	var uid uint = uint(i)
+	err = c.respRepository.Delete(uid)
+	if err != nil {
+		ctx.JSON(400, ctx.Error(err))
+		return
+	}
+	ctx.JSON(200, gin.H{"deleted": true})
+}
+
+//
+// Responsibility Synonims
+//
+
+func (c *respController) FindAllSynonims(ctx *gin.Context) {
+	skills, err := c.respRepository.FindAllResponsibilitySynonim()
+	if err != nil {
+		ctx.JSON(400, ctx.Error(err))
+		return
+	}
+	ctx.JSON(200, skills)
+}
+
+func (c *respController) DeleteSynonim(ctx *gin.Context) {
+	var synonim schemas.ResponsibilitySynonimSchema
+	err := ctx.BindJSON(&synonim)
+	if err != nil {
+		ctx.JSON(400, ctx.Error(err))
+		return
+	}
+	err = c.respRepository.DeleteResponsibilitySynonim(synonim)
+	if err != nil {
+		ctx.JSON(400, ctx.Error(err))
+		return
+	}
+	ctx.JSON(200, gin.H{"deleted": true})
+}
+
+func (c *respController) CreateSynonim(ctx *gin.Context) {
+	var synonim schemas.ResponsibilitySynonimSchema
+
+	err := ctx.BindJSON(&synonim)
+	if err != nil {
+
+		ctx.JSON(400, ctx.Error(err))
+		return
+	}
+
+	synonim, err = c.respRepository.CreateResponsibilitySynonim(synonim)
+	if err != nil {
+		ctx.JSON(400, ctx.Error(err))
+		return
+	}
+	ctx.JSON(200, synonim)
+}
+
+//
+// Responsibility Conflict
+//
+
+func (c *respController) CreateRespConflict(ctx *gin.Context) {
+	var conflict schemas.ResponsibilityConflictSchema
+
+	err := ctx.BindJSON(&conflict)
+	if err != nil {
+
+		ctx.JSON(400, ctx.Error(err))
+		return
+	}
+
+	conflict, err = c.respRepository.CreateResponsibilityConflict(conflict)
+	if err != nil {
+		ctx.JSON(400, ctx.Error(err))
+		return
+	}
+	ctx.JSON(200, conflict)
+}
+
+func (c *respController) FindAllRespConflicts(ctx *gin.Context) {
+	conflicts, err := c.respRepository.FindAllResponsibilityConflicts()
+	if err != nil {
+		ctx.JSON(400, ctx.Error(err))
+		return
+	}
+	ctx.JSON(200, conflicts)
+}
+
+func (c *respController) DeleteRespConflict(ctx *gin.Context) {
+	var conflict schemas.ResponsibilityConflictSchema
+	err := ctx.BindJSON(&conflict)
+	if err != nil {
+		ctx.JSON(400, ctx.Error(err))
+		return
+	}
+	err = c.respRepository.DeleteResponsibilityConflict(conflict)
+	if err != nil {
+		ctx.JSON(400, ctx.Error(err))
+		return
+	}
+	ctx.JSON(200, gin.H{"deleted": true})
+}
