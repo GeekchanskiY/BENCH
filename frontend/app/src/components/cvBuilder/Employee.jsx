d0ab7e2454507163776e4545a3eae45e3684080b
GeekchanskiY <astrovskiy.04@gmail.com>
2024-04-04 01:50:29 +0400

    temp init

--- frontend/app/src/components/cvBuilder/Employee.jsx
+++ frontend/app/src/components/cvBuilder/Employee.jsx
@@ -1,5 +1,6 @@
 import { object, string, number, date } from 'yup';
 import { useEffect, useState } from 'react';
+import { Formik } from 'formik';
 
 let employeeSchema = object({
   id: number().positive().integer(),
@@ -17,19 +18,107 @@ async function fetchUsers(){
     return await validatedObjects
 }
 
-export default function Employee(){
-    const [employees, setEmployees] = useState([
-        {name: 'Dima', age: 12}
-    ])
+function CreateEmployeeForm(){
+    async function createEmployee(data){
+        console.log(data)
+        try {
+            let response = await fetch(
+                'http://0.0.0.0:3001/v1/employee/',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        name: data.name,
+                        age: data.age
+                    })
+                }
+            )
+            // let data = await response.json()
+            // console.log(data)
+        } catch (errors) {
+            console.log(errors)
+            return {
+                success: false
+            }
+        }
+        return {
+            success: true
+        }
+    }
 
-    useEffect(async () => {
+    return <Formik
+      initialValues={{ name: 'Dmitry', age: '12' }}
+      validationSchema={employeeSchema}
+      onSubmit={async (values, { setSubmitting, setErrors }) => {
+
+        let data = await createEmployee(values)
+        if (data.success == true) {
+          setSubmitting(false);
+        } else {
+          setErrors({ 'email': 'Invalid login or password!' })
+        }
+
+      }}>
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+      }) => (
+        <form onSubmit={handleSubmit} className='frm loginform'>
+          <h3>Create Employee</h3>
+          <input
+            type="text"
+            name="name"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.name}
+          /> <br />
+          <span className='errors'>{errors.name && touched.name && errors.name}</span> <br />
+          <input
+            type="number"
+            name="age"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.age}
+          /> <br />
+          <span className='errors'>{errors.age && touched.age && errors.age}</span> <br />
+          <button type="submit" disabled={isSubmitting}>
+            Create
+          </button>
+        </form>
+      )}
+    </Formik>
+}
+
+
+function EmployeeComponent(props){
+    return <div className='cv_instance'>
+        <span>ID: {props.employee.id}</span> <br />
+        <span>Name: {props.employee.name}</span> <br />
+        <span>Age: {props.employee.age}</span>    
+    </div>
+}
+
+export default function Employee(){
+    const [employees, setEmployees] = useState([])
+    async function setUp(){
         setEmployees(await fetchUsers())
+    }
+    useEffect(() => {
+        setUp()
     }, [])
     
     return <div className="cv_model">
         <h1>Employees</h1>
         {employees.map((employee)=>{
-            return <span>{employee.name}</span>
+            return <EmployeeComponent employee={employee}/>
         })}
+        <CreateEmployeeForm></CreateEmployeeForm>
     </div>
 }
