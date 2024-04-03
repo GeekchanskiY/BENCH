7e336d256cb02bbe77ac4aa032d5705981ae5e47
GeekchanskiY <astrovskiy.04@gmail.com>
2024-04-04 01:33:17 +0400

    Cors added

--- finance/routers/routers.go
+++ finance/routers/routers.go
@@ -9,8 +9,25 @@ import (
 	"gorm.io/gorm"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func SetupRouter(db *gorm.DB) *gin.Engine {
 	r := gin.Default()
+	r.Use(CORSMiddleware())
 	docs.SwaggerInfo.BasePath = "/v1"
 	v1 := r.Group("/v1")
 	{
