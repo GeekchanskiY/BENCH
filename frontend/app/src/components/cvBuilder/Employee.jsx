d189363242959440f82af9d06d49b8f625368dca
GeekchanskiY <astrovskiy.04@gmail.com>
2024-04-08 13:58:08 +0200

    format code

--- frontend/app/src/components/cvBuilder/Employee.jsx
+++ frontend/app/src/components/cvBuilder/Employee.jsx
@@ -8,148 +8,148 @@ let employeeSchema = object({
   age: number().required().positive().integer(),
 });
 
-async function fetchUsers(){
-    let response = await fetch('http://0.0.0.0:3001/v1/employee/')
-    let employees = await response.json()
-    if (employees == null){
-      return []
-    }
-    let validatedObjects = await Promise.all(employees.map(async (object) => {
-        await employeeSchema.validate(object, { abortEarly: false });
-        return object;
-    }));
-    return await validatedObjects
+async function fetchUsers() {
+  let response = await fetch('http://0.0.0.0:3001/v1/employee/')
+  let employees = await response.json()
+  if (employees == null) {
+    return []
+  }
+  let validatedObjects = await Promise.all(employees.map(async (object) => {
+    await employeeSchema.validate(object, { abortEarly: false });
+    return object;
+  }));
+  return await validatedObjects
 }
 
-function CreateEmployeeForm({refresh, setRefresh}){
-    async function createEmployee(data){
-        console.log(data)
-        try {
-            let response = await fetch(
-                'http://0.0.0.0:3001/v1/employee/',
-                {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        name: data.name,
-                        age: data.age
-                    })
-                }
-            )
-            //let data = await response.json()
-            //console.log(data)
-            setRefresh(!refresh)
-        } catch (errors) {
-            console.log(errors)
-            return {
-                success: false
-            }
-        }
-        return {
-            success: true
-        }
-    }
-
-    return <Formik
-      initialValues={{ name: 'Dmitry', age: 20 }}
-      validationSchema={employeeSchema}
-      onSubmit={async (values, { setSubmitting, setErrors }) => {
-
-        let data = await createEmployee(values)
-        if (data.success == true) {
-          setSubmitting(false);
-        } else {
-          setErrors({ 'email': 'Invalid login or password!' })
-        }
-
-      }}>
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-      }) => (
-        <form onSubmit={handleSubmit} className='frm loginform'>
-          <h3>Create Employee</h3>
-          <input
-            type="text"
-            name="name"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.name}
-          /> <br />
-          <span className='errors'>{errors.name && touched.name && errors.name}</span> <br />
-          <input
-            type="number"
-            name="age"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.age}
-          /> <br />
-          <span className='errors'>{errors.age && touched.age && errors.age}</span> <br />
-          <button type="submit" disabled={isSubmitting}>
-            Create
-          </button>
-        </form>
-      )}
-    </Formik>
-}
-
-
-function EmployeeComponent(props){
-    async function deleteEmployee(){
+function CreateEmployeeForm({ refresh, setRefresh }) {
+  async function createEmployee(data) {
+    console.log(data)
+    try {
       let response = await fetch(
-        'http://0.0.0.0:3001/v1/employee/'+props.employee.id,
+        'http://0.0.0.0:3001/v1/employee/',
         {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json'
-            },
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            name: data.name,
+            age: data.age
+          })
         }
-        
       )
-      props.setRefresh(!props.refresh)
+      //let data = await response.json()
+      //console.log(data)
+      setRefresh(!refresh)
+    } catch (errors) {
+      console.log(errors)
+      return {
+        success: false
+      }
     }
-    return <div className='cv_instance'>
-        <ul>
-          <li>ID: {props.employee.id}</li>
-          <li>Name: {props.employee.name}</li>
-          <li>Age: {props.employee.age}</li>
-        </ul>
-        <button onClick={deleteEmployee}>Delete</button>   
-    </div>
+    return {
+      success: true
+    }
+  }
+
+  return <Formik
+    initialValues={{ name: 'Dmitry', age: 20 }}
+    validationSchema={employeeSchema}
+    onSubmit={async (values, { setSubmitting, setErrors }) => {
+
+      let data = await createEmployee(values)
+      if (data.success == true) {
+        setSubmitting(false);
+      } else {
+        setErrors({ 'email': 'Invalid login or password!' })
+      }
+
+    }}>
+    {({
+      values,
+      errors,
+      touched,
+      handleChange,
+      handleBlur,
+      handleSubmit,
+      isSubmitting,
+    }) => (
+      <form onSubmit={handleSubmit} className='frm loginform'>
+        <h3>Create Employee</h3>
+        <input
+          type="text"
+          name="name"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.name}
+        /> <br />
+        <span className='errors'>{errors.name && touched.name && errors.name}</span> <br />
+        <input
+          type="number"
+          name="age"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.age}
+        /> <br />
+        <span className='errors'>{errors.age && touched.age && errors.age}</span> <br />
+        <button type="submit" disabled={isSubmitting}>
+          Create
+        </button>
+      </form>
+    )}
+  </Formik>
 }
 
-export default function Employee(){
-    const [employees, setEmployees] = useState([])
-    const [refresh, setRefresh] = useState(false)
-    async function setUp(){
-        setEmployees(await fetchUsers())
-    }
-    useEffect(() => {
-        setUp()
-    }, [refresh])
-    console.log(employees)
-    if (employees.length == 0){
-      return <div className="cv_model">
-        <h1>Employees</h1>
-        <span>No records found!</span>
-        <CreateEmployeeForm refresh={refresh} setRefresh={setRefresh}/>
-      </div>
-    } 
+
+function EmployeeComponent(props) {
+  async function deleteEmployee() {
+    let response = await fetch(
+      'http://0.0.0.0:3001/v1/employee/' + props.employee.id,
+      {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      }
+
+    )
+    props.setRefresh(!props.refresh)
+  }
+  return <div className='cv_instance'>
+    <ul>
+      <li>ID: {props.employee.id}</li>
+      <li>Name: {props.employee.name}</li>
+      <li>Age: {props.employee.age}</li>
+    </ul>
+    <button onClick={deleteEmployee}>Delete</button>
+  </div>
+}
+
+export default function Employee() {
+  const [employees, setEmployees] = useState([])
+  const [refresh, setRefresh] = useState(false)
+  async function setUp() {
+    setEmployees(await fetchUsers())
+  }
+  useEffect(() => {
+    setUp()
+  }, [refresh])
+  console.log(employees)
+  if (employees.length == 0) {
     return <div className="cv_model">
-        <h1>Employees</h1>
-        <div className='cv_instances'>
-          {employees.map((employee)=>{
-              return <EmployeeComponent employee={employee} key={employee.id} refresh={refresh} setRefresh={setRefresh}/>
-          })}
-        </div>
-        <CreateEmployeeForm refresh={refresh} setRefresh={setRefresh}></CreateEmployeeForm>
+      <h1>Employees</h1>
+      <span>No records found!</span>
+      <CreateEmployeeForm refresh={refresh} setRefresh={setRefresh} />
     </div>
-    
+  }
+  return <div className="cv_model">
+    <h1>Employees</h1>
+    <div className='cv_instances'>
+      {employees.map((employee) => {
+        return <EmployeeComponent employee={employee} key={employee.id} refresh={refresh} setRefresh={setRefresh} />
+      })}
+    </div>
+    <CreateEmployeeForm refresh={refresh} setRefresh={setRefresh}></CreateEmployeeForm>
+  </div>
+
 }
--- frontend/app/src/components/cvBuilder/Skill.jsx
+++ frontend/app/src/components/cvBuilder/Skill.jsx
@@ -6,48 +6,48 @@ let skillSchema = object().shape({
     id: number().positive().integer(),
     name: string().required(),
     description: string().required()
-  });
+});
 
 
-  let skillDomainSchema = object().shape({
+let skillDomainSchema = object().shape({
     skill_id: number().positive().integer().required(),
     domain_id: number().positive().integer().required(),
     priority: number().positive().integer().required()
-  });
+});
 
-  let skillConflictSchema = object().shape({
+let skillConflictSchema = object().shape({
     skill_1: number().positive().integer().required(),
     skill_2: number().positive().integer().required(),
     priority: number().positive().integer().required(),
     comment: string().required()
-  });
+});
 
-  let skillDependencySchema = object().shape({
+let skillDependencySchema = object().shape({
     parent_skill: number().positive().integer().required(),
     child_skill: number().positive().integer().required(),
     priority: number().positive().integer().required()
-  });
+});
 
 
-async function fetchSkills(){
+async function fetchSkills() {
     let response = await fetch('http://0.0.0.0:3001/v1/skill/')
     let skills = await response.json()
-    if (skills == null){
-      return []
+    if (skills == null) {
+        return []
     }
     let validatedObjects = await Promise.all(skills.map(async (object) => {
         await skillSchema.validate(object, { abortEarly: false });
         return object;
     }));
     return await validatedObjects
-  }
+}
 
-  function CreateSkillForm({refresh, setRefresh}){
-    async function createSkill(data){
-        
+function CreateSkillForm({ refresh, setRefresh }) {
+    async function createSkill(data) {
+
         try {
-          let res = await skillSchema.validate(data, { abortEarly: false });
-          console.log(res)
+            let res = await skillSchema.validate(data, { abortEarly: false });
+            console.log(res)
             let response = await fetch(
                 'http://0.0.0.0:3001/v1/skill/',
                 {
@@ -71,55 +71,55 @@ async function fetchSkills(){
     }
 
     return <Formik
-      initialValues={{ 
-        name: 'Python',
-        description: 'Programming language'
-       }}
-      validationSchema={skillSchema}
-      onSubmit={async (values, { setSubmitting, setErrors }) => {
+        initialValues={{
+            name: 'Python',
+            description: 'Programming language'
+        }}
+        validationSchema={skillSchema}
+        onSubmit={async (values, { setSubmitting, setErrors }) => {
 
-        let data = await createSkill(values)
-        if (data.success == true) {
-          setSubmitting(false);
-        } else {
-          setErrors({ 'name': 'error!' })
-        }
+            let data = await createSkill(values)
+            if (data.success == true) {
+                setSubmitting(false);
+            } else {
+                setErrors({ 'name': 'error!' })
+            }
 
-      }}>
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-      }) => (
-        <form onSubmit={handleSubmit} className='frm loginform'>
-          <h3>Create Skill</h3>
-          <input
-            type="text"
-            name="name"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.name}
-          /> <br />
-          <span className='errors'>{errors.name && touched.name && errors.name}</span> <br />
-         <input type="text" name="description" onChange={handleChange} onBlur={handleBlur} value={values.description} /> <br />
-          <span className='errors'>{errors.description && touched.description && errors.description}</span> <br />
-         <button type="submit" disabled={isSubmitting}>
-            Create
-          </button>
-        </form>
-      )}
+        }}>
+        {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+        }) => (
+            <form onSubmit={handleSubmit} className='frm loginform'>
+                <h3>Create Skill</h3>
+                <input
+                    type="text"
+                    name="name"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.name}
+                /> <br />
+                <span className='errors'>{errors.name && touched.name && errors.name}</span> <br />
+                <input type="text" name="description" onChange={handleChange} onBlur={handleBlur} value={values.description} /> <br />
+                <span className='errors'>{errors.description && touched.description && errors.description}</span> <br />
+                <button type="submit" disabled={isSubmitting}>
+                    Create
+                </button>
+            </form>
+        )}
     </Formik>
 }
 
-export function SkillDomain(props){
-    async function createSkillDomain(data){
+export function SkillDomain(props) {
+    async function createSkillDomain(data) {
         try {
-          let res = await skillSchema.validate(data, { abortEarly: false });
-          console.log(res)
+            let res = await skillSchema.validate(data, { abortEarly: false });
+            console.log(res)
             let response = await fetch(
                 'http://0.0.0.0:3001/v1/skill/',
                 {
@@ -146,11 +146,11 @@ export function SkillDomain(props){
     </div>
 }
 
-export function SkillDependency(props){
-    async function createSkillDependency(data){
+export function SkillDependency(props) {
+    async function createSkillDependency(data) {
         try {
-          let res = await skillDependencySchema.validate(data, { abortEarly: false });
-          console.log(res)
+            let res = await skillDependencySchema.validate(data, { abortEarly: false });
+            console.log(res)
             let response = await fetch(
                 'http://0.0.0.0:3001/v1/skill/',
                 {
@@ -177,14 +177,14 @@ export function SkillDependency(props){
     </div>
 }
 
-export function SkillConflict(){
+export function SkillConflict() {
     return <div>
         <h3>Conflicts</h3>
     </div>
 }
 
-function SkillComponent(props){
-    async function deleteSkill(){
+function SkillComponent(props) {
+    async function deleteSkill() {
         await fetch(
             'http://0.0.0.0:3001/v1/skill/' + props.skill.id,
             {
@@ -193,43 +193,43 @@ function SkillComponent(props){
                     'Content-Type': 'application/json'
                 },
             }
-            
+
         )
-      props.setRefresh(!props.refresh)
+        props.setRefresh(!props.refresh)
     }
     return <div className='cv_instance'>
         <ul>
-          <li>ID: {props.skill.id}</li>
-          <li>Name: {props.skill.name}</li>
-          <li>Description: {props.skill.description}</li>
+            <li>ID: {props.skill.id}</li>
+            <li>Name: {props.skill.name}</li>
+            <li>Description: {props.skill.description}</li>
         </ul>
-        <button onClick={deleteSkill}>Delete</button>   
+        <button onClick={deleteSkill}>Delete</button>
     </div>
-  }
+}
 
-export default function Skill(){
+export default function Skill() {
     const [skills, setSkills] = useState([])
     const [refresh, setRefresh] = useState(false)
-    async function setUp(){
+    async function setUp() {
         setSkills(await fetchSkills())
     }
     useEffect(() => {
         setUp()
     }, [refresh])
     console.log(skills)
-    if (skills.length == 0){
-      return <div className="cv_model">
-        <h1>Skills</h1>
-        <span>No records found!</span>
-        <CreateSkillForm refresh={refresh} setRefresh={setRefresh}/>
-      </div>
-    } 
+    if (skills.length == 0) {
+        return <div className="cv_model">
+            <h1>Skills</h1>
+            <span>No records found!</span>
+            <CreateSkillForm refresh={refresh} setRefresh={setRefresh} />
+        </div>
+    }
     return <div className="cv_model">
         <h1>Skills</h1>
         <div className='cv_instances'>
-          {skills.map((skill)=>{
-              return <SkillComponent skill={skill} key={"skill_"+skill.id} refresh={refresh} setRefresh={setRefresh}/>
-          })}
+            {skills.map((skill) => {
+                return <SkillComponent skill={skill} key={"skill_" + skill.id} refresh={refresh} setRefresh={setRefresh} />
+            })}
         </div>
         <SkillDomain refresh={refresh} setRefresh={setRefresh}></SkillDomain>
         <SkillConflict refresh={refresh} setRefresh={setRefresh}></SkillConflict>
