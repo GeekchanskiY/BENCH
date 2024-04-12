f812d5dba2bc877f8fa620d4f3106e365d0880e7
GeekchanskiY <astrovskiy.04@gmail.com>
2024-04-12 20:38:59 +0400

    temp upd

--- frontend/app/src/components/cvBuilder/Vacancy.jsx
+++ frontend/app/src/components/cvBuilder/Vacancy.jsx
@@ -95,7 +95,7 @@ function CreateVacancyForm({ refresh, setRefresh }) {
             isSubmitting,
         }) => (
             <form onSubmit={handleSubmit} className='frm loginform'>
-                <h3>Create Skill</h3>
+                <h3>Create Vacancy</h3>
                 <input
                     type="text"
                     name="name"
@@ -123,21 +123,247 @@ function CreateVacancyForm({ refresh, setRefresh }) {
 }
 
 
-export function VacancySkill(){
+export function VacancySkill(props){
+    const [vacancySkills, setVacancySkills] = useState([])
+    async function fetchSkills() {
+        let response = await fetch('http://0.0.0.0:3001/v1/vacancy/'+ props.vacancy_id +'/skills')
+        let skills = await response.json()
+        if (skills == null) {
+            return []
+        }
+        let validatedObjects = await Promise.all(skills.map(async (object) => {
+            await vacancySkillSchema.validate(object, { abortEarly: false });
+            return object;
+        }));
+        return validatedObjects;
+    }
+
+    async function setUp(){
+        setVacancySkills(await fetchSkills())
+    }
+    useEffect(() => {
+        setUp()
+    }, [])
+
+    async function createVacancySkill(data) {
+        try {
+            let res = await vacancySkillSchema.validate(data, { abortEarly: false });
+            
+            await fetch(
+                'http://0.0.0.0:3001/v1/vacancy/skill',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(res)
+                }
+            )
+            setUp()
+        } catch (errors) {
+            return {
+                success: false
+            }
+        }
+        return {
+            success: true
+        }
+    }
+
+    async function deleteVacancySkill(data) {
+        await fetch(
+            'http://0.0.0.0:3001/v1/vacancy/skill',
+            {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            }
+        )
+        setUp()
+    }
+
     return <div>
         <h1>Skills</h1>
     </div>
 }
-export function VacancyDomain(){
+export function VacancyDomain(props){
+    const [vacancyDomain, setVacancyDomain] = useState([])
+    async function fetchVacancyDomain(){
+        let response = await fetch(
+            'http://0.0.0.0:3001/v1/vacancy/' + props.vacancy_id + '/domains',
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            }
+        )
+        let vacancydomain = await response.json()
+        if (vacancydomain === null) {
+            return []
+        }
+        let validatedObjects = await Promise.all(vacancydomain.map(async (object) => {
+            await vacancyDomainSchema.validate(object, { abortEarly: false });
+            return object;
+        }));
+        return validatedObjects;
+    }
+
     return <div>
         <h1>Domains</h1>
+        <div className='cv_instances popup_instance'>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Vacancy</th>
+                        <th>Skill</th>
+                        <th>Priority</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {vacancySkills.map((vacancySkill, index) => (
+                        <tr key={"vacancy_skill_table_item_"+index}>
+                            <td>{vacancySkill.vacancy_id}</td>
+                            <td>{vacancySkill.skill_id}</td>
+                            <td>{vacancySkill.priority}</td>
+                            <td><button onClick={() => deleteVacancySkill(vacancySkill)}>Delete</button></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+        <Formik
+            initialValues={{
+                skill_id: 1,
+                vacancy_id: 2,
+                priority: 1,
+            }}
+            validationSchema={vacancySkillSchema}
+            onSubmit={async (values, { setSubmitting, setErrors }) => {
+
+                let data = await createVacancySkill(values)
+                if (data.success == true) {
+                    setSubmitting(false);
+                } else {
+                    setErrors({ 'comment': 'error!' })
+                }
+
+            }}>
+            {({
+                values,
+                errors,
+                touched,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting,
+            }) => (
+                <form onSubmit={handleSubmit} className='frm '>
+                    <h3>Create Vacancy Skill</h3>
+                    <input
+                        type="text"
+                        name="skill_id"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.skill_id}
+                    /> <br />
+                    <span className='errors'>{errors.skill_id && touched.skill_id && errors.skill_id}</span> <br />
+                    <input
+                        type="text"
+                        name="vacancy_id"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.vacancy_id}
+                    /> <br />
+                    <span className='errors'>{errors.vacancy_id && touched.vacancy_id && errors.vacancy_id}</span> <br />
+                    <input
+                        type="number"
+                        name="priority"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.priority}
+                    /> <br />
+                    <span className='errors'>{errors.priority && touched.priority && errors.priority}</span> <br />
+                    <button type="submit" disabled={isSubmitting}>
+                        Create
+                    </button>
+                </form>
+            )}
+        </Formik>
     </div>
 }
 
 export function VacancyComponent(props){
-    return <div className='cv_instance'> 
-        {props.vacancy.name}
-    </div>
+
+    const [domains, setDomains] = useState(null)
+    const [skills, setSkills] = useState(null)
+
+    async function getVacancyDomains() {
+        const response = await fetch(
+            'http://0.0.0.0:3001/v1/vacancy/' + props.vacancy.id + '/domains',
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            }
+        )
+        const data = await response.json()
+        setDomains(data)
+        return data
+    }
+
+    async function getVacancySkills() {
+        const response = await fetch(
+            'http://0.0.0.0:3001/v1/vacancy/' + props.vacancy.id + '/skills',
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            }
+        )
+        const data = await response.json()
+        setSkills(data)
+        return data
+    }
+
+    useEffect(() => {
+        getVacancyDomains()
+        getVacancySkills()
+    }, [])
+
+    async function deleteVacancy(){
+        await fetch(
+            'http://0.0.0.0:3001/v1/vacancy/' + props.vacancy.id,
+            {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            }
+        )
+        props.setRefresh(!props.refresh)
+    }
+
+    function callPopup(caller) {
+        props.setPopup(caller, props.vacancy.id)
+    }
+
+    return <tr>
+    <td><input type="checkbox" name={'vacancy_' + props.vacancy.id} /></td>
+    <td>{props.vacancy.id}</td>
+    <td>{props.vacancy.name}</td>
+    <td>{props.vacancy.description}</td>
+    <td className={domains !== null ? "clickable_row" : ""} onClick={() => callPopup("vacancy_domain")}>{domains === null ? "-" : domains.length}</td>
+    <td className={skills !== null ? "clickable_row" : ""} onClick={() => callPopup("vacancy_skill")}>{skills === null ? "-" : skills.length}</td>
+    
+    <td>
+      <button onClick={deleteVacancy}>Delete</button>
+    </td>
+  </tr>
 
 }
 
@@ -170,9 +396,13 @@ export default function Vacancy(){
         console.log(vacancy)
         console.log(new_popup)
         switch (new_popup) {
-            case "skill_conflict":
+            case "vacancy_domain":
                 
-                setPopupBlock()
+                setPopupBlock(<VacancyDomain vacancy_id={vacancy} refresh={refresh} setRefresh={setRefresh} />)
+                break;
+            case "vacancy_skill":
+                
+                setPopupBlock(<VacancySkill vacancy_id={vacancy} refresh={refresh} setRefresh={setRefresh} />)
                 break;
             default:
                 setPopupBlock(<span>Empty</span>)
@@ -195,8 +425,7 @@ export default function Vacancy(){
                         <th>Name</th>
                         <th>Description</th>
                         <th>Domains</th>
-                        <th>Dependencies</th>
-                        <th>Conflicts</th>
+                        <th>Skills</th>
                         <th>Actions</th>
                     </tr>
                 </thead>
